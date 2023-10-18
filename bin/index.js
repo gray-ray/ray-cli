2c65d4af335c89d6151ba77a7ab401b8f08629f0
张全 <72014072+gray-ray@users.noreply.github.com>
2023-10-18 16:58:00 +0800

    fix: 文件引入问题

--- bin/index.js
+++ bin/index.js
@@ -9,21 +9,15 @@ const {PROJECT_TEMPLATES}  = require('../lib/constants');
 
 const { initProject, dirExists, cloneTemplate } = require('../lib/fn');
 
-const pkg = require('../package.json');
-
-
-
 /**
  * @description 快捷查询
  */
-program.version(pkg.version, '-v, --version');
+program.version(require('../package.json').version, '-v, --version');
 
-program.option('-h, --help');
 /**
  * @description 版本检查
  */
 program
-.version(pkg.version)
 .command('check')
 .description('check version').action(() => {
   update();
@@ -35,7 +29,6 @@ program
  */
 
 program
-  .version(pkg.version)
   .command('create <projectName>')
   .description('create a new project')
   .action(async (projectName, params) => {
@@ -46,46 +39,38 @@ program
     
     const res = await cloneTemplate(projectName, PROJECT_TEMPLATES[answers?.template]);
 
-
-
-    
-
   });
 
 
 
 
-// TODO: commander 快捷键生成页面 
 
-program
-  .version(pkg.version)
-  .command('monorepo <name>')
-  .description('create a new monorepo project')
-  .action(async (name, params) => {
+// program
+//   .command('monorepo <name>')
+//   .description('create a new monorepo project')
+//   .action(async (name, params) => {
 
-    if(!shell.which('pnpm')) {
-      shell.exec('npm i -g pnpm');
-    }
+//     if(!shell.which('pnpm')) {
+//       shell.exec('npm i -g pnpm');
+//     }
 
-    // TODO: 测试暂时使用
-    shell.exec(`rm -rf ${name}`);
+//     // TODO: 测试暂时使用
+//     shell.exec(`rm -rf ${name}`);
 
-    const isExists =  dirExists(name);
-    if(isExists) return;
+//     const isExists =  dirExists(name);
+//     if(isExists) return;
     
 
 
-    fs.writeFileSync()
+//     fs.writeFileSync()
  
-    // TODO:    权限不足如何处理
-    shell.mkdir(name);
+//     // TODO:    权限不足如何处理
+//     shell.mkdir(name);
     
-    shell.exec(`cd ./${name} && pnpm init`);
+//     shell.exec(`cd ./${name} && pnpm init`);
   
-    shell.touch(`./${name}/pnpm-workspace.yaml`)
+//     shell.touch(`./${name}/pnpm-workspace.yaml`)
 
-
-
-  });
+//   });
 
 program.parse(process.argv);
